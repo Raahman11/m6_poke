93d28c7f22da51a0da9f24dc9068de5dfb2c4fe2
derogate <afiqf.as@gmail.com>
2021-05-10 12:12:26 +0800

    corrected size add randomize

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var pokeResults = document.getElementsByClassName('pokechecker')[0];
 var inputStr = document.getElementById('search').value;
 var inputNum = parseInt(inputStr);
+const pokemonsNumForRandomize = 899;
 
 function consoleInput(){
 	let inputStrConsole = document.getElementById('search').value;
@@ -45,8 +46,10 @@ async function getPokemon() {
 	const pokemonMovesForPP = pokemon.moves.map(skill =>{
 		return `${skill.move.name}`
 	});
+	/*
 	//GETTING MOVESDETAILS BY  INDIVIDUAL MOVES THEN PUSHING EACH SPECIFIED RESULTS INTO AN ARRAY
 	var movesDetails = [];
+	
 	const fetchPokemonMoves = async function(){
 		for(let i = 0; i < pokemonMovesForPP.length; i++){
 			const url = `https://pokeapi.co/api/v2/move/${pokemonMovesForPP[i]}`;
@@ -54,36 +57,27 @@ async function getPokemon() {
 			movesDetails.push(data);
 			var info = movesDetails;
 		}
+		
 		//INDIVIDUAL MOVES DETAILS (for checking purposes)
 		for await (const moves of movesDetails){
 			//console.log(moves) //PROTIP: hover on <-- this for loop movesDetails[i] in VSCode to see the JSON tooltip!!
 		}
-	
 		for(let i = 0; i <pokemonMovesForPP.length; i++){
-			console.log(info[i]) 
+			console.log(movesDetails) 
 		}
+		let raw = fetchPokemonMoves();
+		console.log(raw)
+		
 	}
-	let raw = fetchPokemonMoves();
-	console.log(raw)
-
-
-
-	
-	
+	*/
 	/*
 	for(let j = 0; j < pokemonMovesForPP.length; j++){
 		console.log(`${movesDetails.pp}`)
 	}
 	*/
 	const pokemonMoves = pokemon.moves.map(skill => {
-		return `<li class="movesList>${skill.move.name} (<b>PP: </b>${movesDetails.name})</li>`
+		return `<li class="movesList>${skill.move.name}</li>`
 	}).join("");
-	/*
-	for(let i = 0; i <= pokemonMovesForPP.length; i++){
-		var li = document.getElementsByClassName('movesList')[i];
-		//li.appendChild()
-	}
-	*/
 
 	const pokemonHeight = ((pokemon.height)/10).toFixed(1);
 	const pokemonWeight = ((pokemon.weight)/10).toFixed(1);
@@ -133,3 +127,9 @@ document.querySelector('.getPokemon').addEventListener('click', function (){
 		getPokemon();
 	}
 });
+
+function randomize(){
+	var randomNum = Math.floor(Math.random()* pokemonsNumForRandomize);
+	document.querySelector('#search').value = randomNum;
+	document.getElementsByClassName('getPokemon')[0].click();
+}
